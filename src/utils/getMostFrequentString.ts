fc1c873f82dd81f4c0cb0bd7923c41eb165fdc93
RafaelMatos <rafael-matos@outlook.com>
2023-12-31 09:38:31 -0300

    profile : created user profile route

---
+++ src/utils/getMostFrequentString.ts
@@ -0,0 +1,12 @@
+export function getMostFrequentString(array: string[]) {
+  const hashmap = array.reduce(
+    (acc, val) => {
+      acc[val] = (acc[val] || 0) + 1
+      return acc
+    },
+    {} as Record<string, number>,
+  )
+  return Object.keys(hashmap).reduce((a, b) =>
+    hashmap[a] > hashmap[b] ? a : b,
+  )
+}
